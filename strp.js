8f3485bfb2c38e2787fe98372c71701c5e108df2
insanehong <intro957@gmail.com>
2013-06-08 21:58:36 +0900

    modify tab string

--- strp.js
+++ strp.js
@@ -1,13 +1,13 @@
-	/**
- * strp
- *
- * Copyright (c) 2013 Insanehong
- *
- * Contributors
- *
- * Licensed under the MIT license.
- * https://github.com/insanehong/strp/blob/master/LICENSE
- */
+/**
+* strp
+*
+* Copyright (c) 2013 Insanehong
+*
+* Contributors
+*
+* Licensed under the MIT license.
+* https://github.com/insanehong/strp/blob/master/LICENSE
+*/
 
 var strp = module.exports = {};
 
@@ -46,36 +46,30 @@ var lpad = strp.lpad = function(str,maxlength,padstr) {
 	var i = 0,
 			s = str;
 
-	if( str.length < maxlength) {
-		for ( ; i < (maxlength - str.length) ; i++ ) {
-			s = padstr.charAt(0).concat(s);
-		}
-	}
-  return s;
+	if( str.length < maxlength) for ( ; i < (maxlength - str.length) ; i++ ) s = padstr.charAt(0).concat(s);
+		
+	return s;
 };
 
 var rpad = strp.rpad = function(str,maxlength,padstr) {
 	var i = 0,		
 			s = str;
 
-	if( str.length < maxlength) {
-		for ( ; i < (maxlength - str.length) ; i++ ) {
-			s  = s.concat(padstr.charAt(0));
-		}
-	}
+	if( str.length < maxlength) for ( ; i < (maxlength - str.length) ; i++ ) s  = s.concat(padstr.charAt(0));
+
 	return s;
 };
 
 var lcfirst = strp.lcfirst = function(str) {
-	 var s = str,
-       first = s.charAt(0).toLowerCase();
-   
-   return first.concat(s.slice(1));
+	var s = str,
+	first = s.charAt(0).toLowerCase();
+
+	return first.concat(s.slice(1));
 };
 
 var ucfirst = strp.ucfirst = function(str) {
-	 var s = str,
-       first = s.charAt(0).toUpperCase();
-   
-   return first.concat(s.slice(1));
+	var s = str,
+	first = s.charAt(0).toUpperCase();
+
+	return first.concat(s.slice(1));
 };
